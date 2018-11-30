f3ee32d86da070876caaccf531736572afc32e30
Luka Logar <luka.logar@iname.com>
2018-11-30 13:34:41 +0100

    IsoApplet: Set appropriate SC_PKCS15_PRKEY_ACCESS_* flags
    Set appropriate SC_PKCS15_PRKEY_ACCESS_* flags when generating/importing private keys.

--- src/pkcs15init/pkcs15-isoApplet.c
+++ src/pkcs15init/pkcs15-isoApplet.c
@@ -637,6 +637,11 @@ isoApplet_generate_key(sc_profile_t *profile, sc_pkcs15_card_t *p15card,
 		sc_log(card->ctx, "%s: Key generation failed: Unknown/unsupported key type.", strerror(r));
 	}
 
+	key_info->access_flags = SC_PKCS15_PRKEY_ACCESS_NEVEREXTRACTABLE
+	        | SC_PKCS15_PRKEY_ACCESS_ALWAYSSENSITIVE
+	        | SC_PKCS15_PRKEY_ACCESS_SENSITIVE
+	        | SC_PKCS15_PRKEY_ACCESS_LOCAL;
+
 	LOG_FUNC_RETURN(card->ctx, r);
 }
 
@@ -787,6 +792,9 @@ isoApplet_store_key(sc_profile_t *profile, sc_pkcs15_card_t *p15card, sc_pkcs15_
 		sc_log(card->ctx, "%s: Error in card_ctl", sc_strerror(r));
 		LOG_FUNC_RETURN(card->ctx, r);
 	}
+
+	key_info->access_flags = SC_PKCS15_PRKEY_ACCESS_NEVEREXTRACTABLE
+	        | SC_PKCS15_PRKEY_ACCESS_SENSITIVE;
 
 	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
 }
